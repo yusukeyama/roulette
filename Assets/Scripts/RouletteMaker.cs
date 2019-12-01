8909855423ea956a3e75be01abd5bdd0b2b0ee58
yusukeyamanishi <nextcentury2000@gmail.com>
2019-12-01 21:03:31 +0900

    add

---
+++ Assets/Scripts/RouletteMaker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RouletteMaker : MonoBehaviour {
+    [SerializeField] private Transform imageParentTransform;
+    public List<string> choices;
+    [SerializeField] private List<Color> rouletteColors;
+    [SerializeField] private Image rouletteImage;
+    [SerializeField] private RouletteController rController;
+    private void Start () {
+        float ratePerRoulette = 1 / (float) choices.Count;
+        float rotatePerRoulette = 360 / (float) (choices.Count);
+        for (int i = 0; i < choices.Count; i++) {
+            var obj = Instantiate (rouletteImage, imageParentTransform);
+            obj.color = rouletteColors[(choices.Count - 1 - i)];
+            obj.fillAmount = ratePerRoulette * (choices.Count - i);
+            obj.GetComponentInChildren<Text> ().text = choices[(choices.Count - 1 - i)];
+            obj.transform.GetChild (0).transform.rotation = Quaternion.Euler (0, 0, ((rotatePerRoulette / 2) + rotatePerRoulette * i));
+        }
+        rController.SetRoulette();
+        rController.rMaker = this;
+        rController.rotatePerRoulette = rotatePerRoulette;
+        rController.roulette = imageParentTransform.gameObject;
+    }
+}
