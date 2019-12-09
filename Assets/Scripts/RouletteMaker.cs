24b720bd9ddb302df81451436891cb0d91693cc3
yusukeyamanishi <nextcentury2000@gmail.com>
2019-12-09 10:26:13 +0900

    [UP]roulette

--- Assets/Scripts/RouletteMaker.cs
+++ Assets/Scripts/RouletteMaker.cs
@@ -12,23 +12,50 @@ public class RouletteMaker : MonoBehaviour {
     private void Start () {
         float ratePerRoulette = 1 / (float) choices.Count;
         float rotatePerRoulette = 360 / (float) (choices.Count);
-        for (int i = 0; i < choices.Count; i++) {
-            var obj = Instantiate (rouletteImage, imageParentTransform);
+        for (int i = 0; i < choices.Count; i++)
+        {
+            var obj = Instantiate(rouletteImage, imageParentTransform);
             obj.color = rouletteColors[(choices.Count - 1 - i)];
-            if (i == 1)
+            switch (i)
             {
-                obj.fillAmount = 0.95f;
-            } else
-            {
-                obj.fillAmount = ratePerRoulette * (choices.Count - i);
+                case 1:
+                    obj.fillAmount = 0.95f;
+                    break;
+                case 2:
+                    obj.fillAmount = 0.8f;
+                    break;
+                case 3:
+                    obj.fillAmount = 0.5f;
+                    break;
+                case 4:
+                    obj.fillAmount = 0.2f;
+                    break;
+                default:
+                    obj.fillAmount = ratePerRoulette * (choices.Count - i);
+                    break;
             }
-            obj.GetComponentInChildren<Text> ().text = choices[(choices.Count - 1 - i)];
-            if (i == 0)
+            obj.GetComponentInChildren<Text>().text = choices[(choices.Count - 1 - i)];
+
+            switch (i)
             {
-                obj.transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 9.26f);
-            } else
-            {
-                obj.transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, ((rotatePerRoulette / 2) + rotatePerRoulette * i));
+                case 0:
+                    obj.transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 9.26f);
+                    break;
+                case 1:
+                    obj.transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 47.31f);
+                    break;
+                case 2:
+                    obj.transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 127.106f);
+                    break;
+                case 3:
+                    obj.transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, -122.989f);
+                    break;
+                case 4:
+                    obj.transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, -36f);
+                    break;
+                default:
+                    obj.transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, ((rotatePerRoulette / 2) + rotatePerRoulette * i));
+                    break;
             }
         }
         rController.SetRoulette();
